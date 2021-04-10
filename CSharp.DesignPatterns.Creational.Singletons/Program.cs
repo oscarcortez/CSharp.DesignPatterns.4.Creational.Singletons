be298b8bd7b4ab1013259854684cfdb06f5855ed
oscarcortez <oscarkortez@gmail.com>
2021-04-09 20:51:05 -0400

    Add project files.

---
+++ CSharp.DesignPatterns.Creational.Singletons/Program.cs
@@ -0,0 +1,18 @@
+﻿using models;
+using System;
+
+namespace CSharp.DesignPatterns.Creational.Singletons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            var result = s1 == s2 ? "singleton good": "singleton failed :(";
+            // vemos que solo se creo un singleton a pesar de intentar crear otra
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ models/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace models
+{
+    public class Singleton
+    {
+        private Singleton()
+        {
+        }
+
+        private static Singleton instance;
+
+        public static Singleton GetInstance()
+        {
+            //if (instance == null)            
+            //    instance = new Singleton();
+            instance ??= new Singleton();
+
+            return instance;
+        }
+
+        public static void HacerAlgo()
+        {
+        }
+    }
+}
