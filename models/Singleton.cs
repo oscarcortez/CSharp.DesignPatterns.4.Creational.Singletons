5ddd97a49495348fa7674cebd939b9ab528e559c
oscarcortez <oscarkortez@gmail.com>
2021-04-12 11:46:46 -0400

    fixed to one single method

--- models/Singleton.cs
+++ models/Singleton.cs
@@ -10,14 +10,19 @@ namespace models
 
         private static Singleton instance;
 
-        public static Singleton GetInstance()
-        {
+        public static Singleton GetInstance() => instance ??= new Singleton();
+        //{
             //if (instance == null)            
             //    instance = new Singleton();
-            instance ??= new Singleton();
 
-            return instance;
-        }
+            //instance ??= new Singleton();
+
+            //return instance;
+
+            //return instance == null ? instance : new Singleton();
+
+            //return instance ??= new Singleton();
+        //}
 
         public static void HacerAlgo()
         {
